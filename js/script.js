0957070c9d50eeec6edae40d1f1fc89efbba34b6
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2023-01-20 22:28:12 +0100

    Set hover effect
    I set up a hover effect so that the grid divs change color when my mouse passes over them, leaving a
    pixelated trail through the grid. I also added a button to the top of the screen that sends the user a
    asking for the number of sides for the new grid. Once entered, the existing grid will be removed and a new
    grid will be generated in the same total space as before.

--- js/script.js
+++ js/script.js
@@ -2,9 +2,47 @@ const gridContainer = document.getElementById("grid-container");
 for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
     let div = document.createElement("div");
-    div.style.width = "20px";
-    div.style.height = "20px";
+    div.style.width = "60px";
+    div.style.height = "60px";
     div.style.backgroundColor = "blue";
     gridContainer.appendChild(div);
   }
 }
+
+const divs = document.querySelectorAll("#grid-container div");
+divs.forEach((div) => {
+  div.addEventListener("mouseover", function (e) {
+    e.target.classList.add("active");
+    div.style.backgroundColor = "red";
+  });
+  div.addEventListener("mouseout", function (e) {
+    e.target.classList.remove("active");
+  });
+});
+
+const gridSizeBtn = document.getElementById("grid-size-btn");
+gridSizeBtn.addEventListener("click", () => {
+  let gridSize = +prompt(
+    "How many number of squares do you want per side for the new grid?",
+    "20"
+  );
+  if (
+    gridSize !== null &&
+    !isNaN(gridSize) &&
+    gridSize > 0 &&
+    gridSize % 1 === 0
+  ) {
+    gridContainer.innerHTML = "";
+    for (let i = 0; i < gridSize; i++) {
+      for (let j = 0; j < gridSize; j++) {
+        let div = document.createElement("div");
+        div.style.height = "2rem";
+        div.style.width = "2rem";
+        div.style.backgroundColor = "green";
+        gridContainer.appendChild(div);
+      }
+    }
+  } else {
+    alert("Invalid input. Please enter a positive integer.");
+  }
+});
