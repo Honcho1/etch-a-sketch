9bd9bfc7c6d519774c22d4577c111a13acccb863
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2023-01-22 00:47:31 +0100

    Change the total space of the grid
    I changed the total space of the grid container to be 640px. I also set the limit for the user input to a
    maximum of 100.

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ const gridContainer = document.getElementById("grid-container");
 for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
     let div = document.createElement("div");
-    div.style.width = "60px";
-    div.style.height = "60px";
-    div.style.backgroundColor = "blue";
+    div.style.width = "40px";
+    div.style.height = "40px";
+    div.style.border = "1px solid #B2B2B2";
     gridContainer.appendChild(div);
   }
 }
@@ -13,7 +13,7 @@ const divs = document.querySelectorAll("#grid-container div");
 divs.forEach((div) => {
   div.addEventListener("mouseover", function (e) {
     e.target.classList.add("active");
-    div.style.backgroundColor = "red";
+    div.style.backgroundColor = "#000";
   });
   div.addEventListener("mouseout", function (e) {
     e.target.classList.remove("active");
@@ -23,26 +23,46 @@ divs.forEach((div) => {
 const gridSizeBtn = document.getElementById("grid-size-btn");
 gridSizeBtn.addEventListener("click", () => {
   let gridSize = +prompt(
-    "How many number of squares do you want per side for the new grid?",
+    "Enter the number of squares you want per side for the new grid",
     "20"
   );
   if (
     gridSize !== null &&
     !isNaN(gridSize) &&
     gridSize > 0 &&
-    gridSize % 1 === 0
+    gridSize % 1 === 0 &&
+    gridSize <= 100
   ) {
     gridContainer.innerHTML = "";
     for (let i = 0; i < gridSize; i++) {
       for (let j = 0; j < gridSize; j++) {
         let div = document.createElement("div");
-        div.style.height = "2rem";
-        div.style.width = "2rem";
-        div.style.backgroundColor = "green";
+        div.style.height = 640 / gridSize + "px";
+        div.style.width = 640 / gridSize + "px";
+        div.style.border = "1px solid #B2B2B2";
+
+        if (gridSize !== 16) {
+          gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, ${
+            640 / gridSize
+          }px)`;
+          gridContainer.style.gridTemplateRows = `repeat(${gridSize}, ${
+            640 / gridSize
+          }px)`;
+        }
+
+        div.addEventListener("mouseover", (e) => {
+          e.target.classList.add("active");
+          div.style.backgroundColor = "#000";
+        });
+
+        div.addEventListener("mouseout", (e) => {
+          e.target.classList.remove("active");
+        });
+
         gridContainer.appendChild(div);
       }
     }
   } else {
-    alert("Invalid input. Please enter a positive integer.");
+    alert("Invalid input. Please enter a positive integer between 1 and 100.");
   }
 });
